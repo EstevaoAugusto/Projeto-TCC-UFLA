767eff6de554d155603af6b8f534aba380604462
Estevão Augusto <estevaoaugusto2005@gmail.com>
2026-02-07 21:41:41 -0300

    feat: adiciona verificação de arquivo

--- scripts/data_search.py
+++ scripts/data_search.py
@@ -3,11 +3,10 @@ import pandas as pd                 # Biblioteca para manipulação e análise d
 import sys                          # Biblioteca para acessar variaveis e funções que interagem fortemente com o interpretador
 from pathlib import Path            # Biblioteca para a manipulação de caminhos do sistema a nivel orientado a objetos
 import time
+import os
 
 ROOT_DIRECTORY_PATH = Path("__file__").resolve().parent
 PARENT_DIRECTORY_PATH = ROOT_DIRECTORY_PATH.parent
-
-print(PARENT_DIRECTORY_PATH)
 
 if str(PARENT_DIRECTORY_PATH) not in sys.path:
     sys.path.append(str(PARENT_DIRECTORY_PATH))
@@ -20,29 +19,33 @@ if __name__ == "__main__":
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0"}
 
 def web_scrapping(df_link : pd.DataFrame):
+    print(f"{'='*60}")
     alt_text_coluna = [] # Lista que terá EXATAMENTE o mesmo tamanho do df_link
     
     quant_registros = len(df_link)
     
-    print(f"Processando {quant_registros} requisições de download de imagens.")
+    print(f"Processando {quant_registros} requisições de download de imagens.\n")
     
     for i in range(0, quant_registros):
         try:
             registro_atual = df_link.iloc[i]
             id_img = registro_atual['id_img']
             url_img_alvo = registro_atual['link_fonte_imagem_selecionada']
-            req_img = requests.get(url_img_alvo, headers=HEADERS, timeout=15)
+            output = Path(IMAGES_DIRECTORY_PATH) / f"{id_img}.jpg"
 
             print(f"Requisição {i+1}/{quant_registros}: {id_img}.jpg")
+            if output.exists():
+                print(f"Arquivo existe: {output}")
+                pass
 
-            output = Path(IMAGES_DIRECTORY_PATH) / f"{registro_atual['id_img']}.jpg"
+            req_img = requests.get(url_img_alvo, headers=HEADERS, timeout=15)
             output.write_bytes(req_img.content)
             
+            time.sleep(3)
         except Exception as e:
             print(f"Erro no índice {i}: {e}")
             alt_text_coluna.append("ERRO_CONEXAO") # Mantém o alinhamento mesmo no erro
 
-        time.sleep(3)
     print(f"\n{'='*60}")
     
     print("Web Scrapping Finalizado")
